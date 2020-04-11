a69f75a230a5a7fedc28fe79e0fd759fa2bcd1b3
Tiago Costa <tiago.tagcosta@gmail.com>
2020-04-11 23:41:03 +0000

    first commit

---
+++ src/project/Main.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello goorm!");
+    }
+
+}
