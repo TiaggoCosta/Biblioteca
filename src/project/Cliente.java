5d6e72458eeb9e5664e3fba247f6b0a03f3eafc8
Karolina <karolina_cris@hotmail.com>
2020-04-19 17:31:38 -0300

    remove builder from cliente

--- src/project/Cliente.java
+++ src/project/Cliente.java
@@ -6,51 +6,35 @@ public class Cliente {
 	private String email;
 	private Integer id;
 	
-	private Cliente(Builder builder) {
-		this.nome = builder.nome;
-		this.email = builder.email;
-		this.id = builder.id;
-	}
-	
-	public static class Builder {
-		private String nome;
-		private String email;
-		private Integer id;
-		
-		public Builder() {
-			
-		}
-		
-		public Builder withNome(String nome) {
-			this.nome = nome;
-			return this;
-		}
-		
-		public Builder withEmail(String email) {
-			this.email = email;
-			return this;
-		}
-		
-		public Builder withId(Integer id) {
-			this.id = id;
-			return this;
-		}
-		
-		public Cliente build() {
-			return new Cliente(this);
-		}
+	public Cliente(String nome, String email, Integer id) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.id = id;
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public String getEmail() {
 		return email;
 	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public Integer getId() {
 		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	@Override
